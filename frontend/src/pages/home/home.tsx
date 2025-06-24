5c8dd0be5543a3d220c19a0c9bbc7b47b066eca8
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-06-24 19:11:33 +0200

    Implement home page component

---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import CanvasBoard from "../../components/canvas-board/canvas-board";
+
+const Home = () => {
+  return (
+    <div>
+      <CanvasBoard boardId="test" />
+    </div>
+  );
+};
+
+export default Home;
