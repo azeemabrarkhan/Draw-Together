4a9eb015a13b68aead7aae1c47c9fcad6956abc5
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-06-25 02:26:39 +0200

    Update App component to use css module

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import NavBar from "./components/nav-bar/nav-bar";
 import Home from "./pages/home/home";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <NavBar></NavBar>
+      <div className={styles.content}>
+        <Home></Home>
+      </div>
     </>
   );
 }
