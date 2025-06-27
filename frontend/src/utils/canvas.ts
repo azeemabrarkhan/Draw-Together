f2b4e92a22cd85b955afbb7bbec4be71a5af126d
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-06-27 18:47:10 +0200

    Implement drawOnCanvas util function

--- frontend/src/utils/canvas.ts
+++ frontend/src/utils/canvas.ts
@@ -1,3 +1,5 @@
+import { ToolNames } from "../enums/toolNames";
+
 export const getCanvasMouseCoords = (
   e: MouseEvent,
   canvas: HTMLCanvasElement,
@@ -13,4 +15,45 @@ export const getCanvasMouseCoords = (
     x: (rawX - pan.x) / zoom,
     y: (rawY - pan.y) / zoom,
   };
+};
+
+export const drawOnCanvas = (
+  startCursorLocation: { x: number; y: number },
+  currentCursorLocation: { x: number; y: number },
+  canvasContext: CanvasRenderingContext2D | null,
+  toolType: ToolNames,
+  strokeStyle: string,
+  strokeWidth: number
+) => {
+  if (!canvasContext) return;
+
+  canvasContext.strokeStyle = strokeStyle;
+  canvasContext.lineWidth = strokeWidth;
+  canvasContext.beginPath();
+
+  switch (toolType) {
+    case ToolNames.DRAW:
+      canvasContext.moveTo(startCursorLocation.x, startCursorLocation.y);
+      canvasContext.lineTo(currentCursorLocation.x, currentCursorLocation.y);
+      break;
+
+    case ToolNames.CIRCLE:
+      const width = currentCursorLocation.x - startCursorLocation.x;
+      const height = currentCursorLocation.y - startCursorLocation.y;
+      const radius = Math.sqrt(width ** 2 + height ** 2) / 2;
+      canvasContext.beginPath();
+      canvasContext.arc(
+        startCursorLocation.x + width / 2,
+        startCursorLocation.y + height / 2,
+        radius,
+        0,
+        Math.PI * 2
+      );
+      break;
+
+    default:
+      break;
+  }
+
+  canvasContext.stroke();
 };
