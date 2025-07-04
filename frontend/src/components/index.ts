915e1cbe83e789c4f445a184c49f042bf5e18ad7
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-07-05 00:38:35 +0200

    rename file

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,4 +2,4 @@ export * from "./canvas-board/canvas-board";
 export * from "./canvas-buttons/canvas-buttons";
 export * from "./nav-bar/nav-bar";
 export * from "./tool-bar/tool-bar";
-export * from "./button/buttons";
+export * from "./button/button";
